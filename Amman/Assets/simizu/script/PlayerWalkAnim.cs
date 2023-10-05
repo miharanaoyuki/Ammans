9f79c5de69f5ffc3dae4903104f55d0eeb857067
rikuto <92492980+rurikunn@users.noreply.github.com>
2023-10-05 16:32:41 +0900

    simizu
    anim

--- Amman/Assets/simizu/script/PlayerWalkAnim.cs
+++ Amman/Assets/simizu/script/PlayerWalkAnim.cs
@@ -17,13 +17,54 @@ public class PlayerWalkAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        //前
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            animator.SetTrigger("walk");
+            animator.SetTrigger("run");
         }
         else if (Input.GetKeyUp(KeyCode.W))
         {
             animator.SetTrigger("idle");
         }
+
+        //後ろ
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("run");
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetTrigger("idle");
+        }
+
+        //右
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("run");
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetTrigger("idle");
+        }
+
+        //左
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("run");
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetTrigger("idle");
+        }
+
+        //ジャンプ
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetTrigger("jump");
+        }
+        //else if (Input.GetKeyUp(KeyCode.))
+        //{
+        //    animator.SetTrigger("idle");
+        //}
     }
 }
