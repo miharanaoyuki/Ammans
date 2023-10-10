0bf3dbb5c0a8a6d6f619eb3684567701998c9179
rikuto <92492980+rurikunn@users.noreply.github.com>
2023-10-10 16:33:09 +0900

    simizu
    プレイヤーの移動

--- Amman/Assets/simizu/script/PlayerMove.cs
+++ Amman/Assets/simizu/script/PlayerMove.cs
@@ -1,41 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
 public class PlayerMove : MonoBehaviour
 {
-    //歩く速さ
-    public float speed;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    //アニメーション
+    private Animator anim = null;
 
+    [SerializeField]
+    private float MoveSpeed;//移動スピードを入れるための変数
+
+    private Vector3 Velocity;//CharacterControllerを動かすためのVector3型の変数を作成
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        //animatorコンポーネントを取得
+        anim = gameObject.GetComponent<Animator>();
+        //CharacterControllerを取得
+        controller = GetComponent<CharacterController>();
     }
-
     void Update()
     {
-        //前方移動
-        if (Input.GetKey(KeyCode.W))
+        // 接地しているなら
+        if (controller.isGrounded)
         {
-            transform.position += speed * transform.forward * Time.deltaTime;
+            anim.SetBool("jump", false);
+            Debug.Log("当");
+            // スペースキーでジャンプ
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("当");
+                anim.SetBool("jump", true);
+                // ジャンプ力を設定
+                moveDirection.y = 5;
+            }
+        }
+        else
+        {
+            Debug.Log("外");
+            // 重力計算
+            moveDirection.y -= 8 * Time.deltaTime;
         }
 
-        //後方移動
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
-            transform.position -= speed * transform.forward * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 315, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 225, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 135, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);//0°に向ける
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            controller.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
         }
 
-        //右移動
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += speed * transform.right * Time.deltaTime;
-        }
+        // Cubeを動かす処理
+        controller.Move(moveDirection * Time.deltaTime);
 
-        //左移動
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= speed * transform.right * Time.deltaTime;
-        }
+        //controller.Move(Velocity * Time.deltaTime);//characterControllerをVelocity * 時間の経過分動かす
     }
 }
