135fc51058441acd4fcb858a215f90bd1da1ef79
umauam <h1202116009@hiratagakuen.onmicrosoft.com>
2023-10-13 13:40:27 +0900

    tuzi
    HP

---
+++ Amman/Assets/tuzinokuni/New Folder/HP.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HP : MonoBehaviour
+{
+    public static int life = 3;//hpを6にする。SliderのMaxValueとValueはここの値に合わせます
+    private Slider _slider;//Sliderの値を代入する_sliderを宣言
+    public GameObject slider;//体力ゲージに指定するSlider
+    public static int maxHp = 3;
+
+
+    public static int MAX = 3;
+
+
+    //[SerializeField]
+    //private SoundManager soundManager;
+    // Use this for initialization
+    void Start()
+    {
+        life = maxHp;
+        //Debug.Log(life);
+
+        _slider = slider.GetComponent<Slider>();//sliderを取得する
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        _slider.maxValue = maxHp;
+
+        //スライダーの現在値の設定
+
+
+
+        _slider.value = life;
+
+
+       
+        if (life <= 0)
+        {
+
+            life = MAX;
+            die();
+        }
+
+
+    }
+
+    void die()
+    {
+
+        SceneManager.LoadScene("over");
+
+        Debug.Log("バイバイ");
+
+    }
+
+
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("バイバイ");
+            //soundManager.Play("damage");
+            life--;
+        }
+
+
+    }
+}
