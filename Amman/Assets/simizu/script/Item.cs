0dedcf596757a6d302d7445124db510557227299
rikuto <92492980+rurikunn@users.noreply.github.com>
2023-10-16 14:44:53 +0900

    simizu
    itemスクリプト

--- Amman/Assets/simizu/script/Item.cs
+++ Amman/Assets/simizu/script/Item.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public float Speed_Up_cnt;    //どれぐらい速くするか
+    public float Speed_Up_Time;   //元のスピードに戻る時間
+    public float Speed_Down_cnt;  //どれぐらい遅くするか
+    public float Speed_Down_Time; //元のスピードに戻る時間
+    public float Speed_Remove;    //元のスピード
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,7 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 
     void OnTriggerEnter(Collider col)
@@ -24,14 +30,24 @@ public class Item : MonoBehaviour
         if (col.gameObject.tag == "heart")
         {
             Destroy(col.gameObject);
-            PlayerMove.MoveSpeed += 3.0f;
+            PlayerMove.MoveSpeed += Speed_Up_cnt;
+
+            Invoke("Remove", Speed_Up_Time); //〇秒後に"Remove"関数を実行
         }
 
-        //2段ジャンプ
+        //プレイヤーのスピードを遅くする
         if (col.gameObject.tag == "b")
         {
             Destroy(col.gameObject);
-            //
+            PlayerMove.MoveSpeed -= Speed_Down_cnt;
+
+            Invoke("Remove", Speed_Down_Time);
         }
+    }
+
+    void Remove()
+    {
+        Debug.Log("スピード戻った");
+        PlayerMove.MoveSpeed = Speed_Remove; //元に戻す
     }
 }
