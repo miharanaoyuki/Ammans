c41157c4a759b53d32ddf5c831b139b835af1b2d
miharanaoyuki <89573107+miharanaoyuki@users.noreply.github.com>
2023-10-31 16:28:31 +0900

    mainとmain2にシーン移行できるようにした。

---
+++ Amman/Assets/NAO/Scene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public string SceneName;
+
+ 
+	public void OnClickStartButton()
+	{
+		SceneManager.LoadScene(SceneName);
+	}
+
+}
