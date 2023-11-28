8de121f8e7594c83579a242ed3a5626ca528a0e4
rikuto <92492980+rurikunn@users.noreply.github.com>
2023-11-28 16:26:06 +0900

    simizu

--- Amman/Assets/simizu/script/MoveFloor_down.cs
+++ Amman/Assets/simizu/script/MoveFloor_down.cs
@@ -6,23 +6,34 @@ public class MoveFloor_down : MonoBehaviour
 {
     float z = 0.1f;
     float y = 0f;
+    private bool floor_col;
 
     // Start is called before the first frame update
     void Start()
     {
         Invoke("Swith2", 0f);
+        floor_col = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MoveFloor_down_col.floor_col == true)
+        if (floor_col == true)
         {
             Vector3 p = new Vector3(0, y, z);
             transform.Translate(p);
 
             Invoke("Swith3", 2f);
             Invoke("Swith4", 2.3f);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("hit");
+            floor_col = true;
         }
     }
 
