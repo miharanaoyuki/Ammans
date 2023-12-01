0366f412d09cc0c91d09dfdf0900b8c3aac5e1e2
rogia5 <89572671+rogia5@users.noreply.github.com>
2023-12-01 14:42:28 +0900

    tetta

--- Amman/Assets/tetta/script/main4/movecloud.cs
+++ Amman/Assets/tetta/script/main4/movecloud.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class movecloud : MonoBehaviour
 {
-    float x = 0.1f;
-    float y = 0.1f;
-    float z = 0.1f;
-    public float x_move;
-    public float y_move;
-    public float z_move;
+    float x = 0.06f;
 
-    public static float player_moveX;
-    public static float player_moveY;
-    public static float player_moveZ;
+    public static float player_x;
 
     // Start is called before the first frame update
     void Start()
@@ -24,34 +17,29 @@ public class movecloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 p = new Vector3(x, y, z);
+        Vector3 p = new Vector3(x, 0, 0);
         transform.Translate(p);
 
+        if(x < 0)
+        {
+            player_x = x - 0.01f;
+        }
+        if (x > 0)
+        {
+            player_x = x + 0.01f;
+        } 
     }
 
     void Swith()
     {
-        x = x_move;
-        y = y_move;
-        z = z_move;
-
-        player_moveX = x;
-        player_moveY = y;
-        player_moveZ = z;
-
-        Invoke("Swith2", 1.0f);
+        x *= -1.0f;
+        Invoke("Swith2", 3.0f);
     }
 
     void Swith2()
     {
-        x = -x_move;
-        y = -y_move;
-        z = -z_move;
+        x *= -1.0f;
 
-        player_moveX = x;
-        player_moveY = y;
-        player_moveZ = z;
-
-        Invoke("Swith", 1.0f);
+        Invoke("Swith", 3.0f);
     }
 }
