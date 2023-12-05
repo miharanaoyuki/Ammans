6d15c6420a00dba84ce8bd0fac10cca3897e3461
rogia5 <89572671+rogia5@users.noreply.github.com>
2023-12-05 13:38:23 +0900

    tetta

--- Amman/Assets/tetta/script/main4/bird.cs
+++ Amman/Assets/tetta/script/main4/bird.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camera_move4 : MonoBehaviour
+public class bird : MonoBehaviour
 {
     float speed = 0.03f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
