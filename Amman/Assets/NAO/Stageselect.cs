db2ed4379bd101261b421a6123d8778f2a9e7444
Yahhoi <89573107+miharanaoyuki@users.noreply.github.com>
2023-12-05 11:11:13 +0900

    StageSelect 修正

--- Amman/Assets/NAO/Stageselect.cs
+++ Amman/Assets/NAO/Stageselect.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Stageselect : MonoBehaviour
 {
-    
-    [SerializeField] GameObject  main2;
-    [SerializeField] GameObject  main3;
-    [SerializeField] GameObject  main4;
+    [SerializeField] GameObject  panel2;
+    [SerializeField] GameObject  panel3;
+    [SerializeField] GameObject  panel4;
+    [SerializeField] GameObject   main2;
+    [SerializeField] GameObject   main3;
+    [SerializeField] GameObject   main4;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +20,20 @@ public class Stageselect : MonoBehaviour
     void Update()
     {
         if (clear_check.clear_check_1 == true)
-        { 
+        {
+           panel2.SetActive(true);
             main2.SetActive(true);
         }
 
         if (clear_check.clear_check_2 == true)
-        { 
+        {
+           panel3.SetActive(true);
             main3.SetActive(true);
         }
 
         if (clear_check.clear_check_3 == true)
         {
+           panel4.SetActive(true);
             main4.SetActive(true);
         }
 
